dc39abcf78339f939fc23c9fed5830bf0eda7d23
wesley Mirand <wewesantos@hotmail.com>
2024-03-11 15:58:44 -0300

    2

---
+++ aula2/aula02.js
@@ -0,0 +1,11 @@
+"use strict" //Modo  estrito mode
+
+let nome="bruno" //declara var
+console.log("Wewe Cursos") //imprimindo var
+console.log(nome) //so string texto
+console.log("nome: " + nome)  //juntando var com stringconcatenando 
+
+/* estrito nao consigo usar um var que nao tenha sido declarada,  
+precisa de cuidado na hora de declarar e usar.  
+Somos obrigado a criar um codigo mais limpo .
+node e react stric estrito*/
