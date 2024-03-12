2470ef5b3b907e8079c40ea52ee735c32222ae5e
wesley Mirand <wewesantos@hotmail.com>
2024-03-12 15:43:10 -0300

    2

---
+++ aula3/parte5/aula3ultimo.js
@@ -0,0 +1,15 @@
+"use strict" 
+
+let nome = "wesley"
+nome = "WEWE CURSOS"
+nome = 10
+
+const curso = "Javascript"
+
+curso = "HTML" //Nao vai ser modificado e vai dar erro
+
+console.log(nome)
+
+/* const : var nao var, quando declaro uma const e atribuo um valor. O valor nao pode
+ser mais alterado. 
+*/
