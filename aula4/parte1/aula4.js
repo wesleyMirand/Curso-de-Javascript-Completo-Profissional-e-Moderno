0812192e9cf08ee53ac3c884ff245b553006deb6
wesley Mirand <wewesantos@hotmail.com>
2024-03-14 23:53:16 -0300

    2

--- aula4/parte1/aula4.js
+++ aula4/parte1/aula4.js
@@ -1,3 +1,26 @@
-let num1, num2 = 10
+let num1, num2, num3
+
+num1 = num2 = num3 = 10
 
 console.log(num1)
+console.log(num2)
+console.log(num3)
+
+/* quando agt cria uma var, ela é alocada reservada na memoria, um lixo dentro dela, cria e
+inicializa para nao ter lixo nessa var 
+Posso declarar em linha separadas:
+ let num1 = 0
+let num2 = 0
+ou linhas juntas: 
+let num1, num2 
+E deixando atribuicao para o final
+
+as 3 vao iniciar com valor 10
+let num1, num2, num3
+
+num1 = num2 = num3 = 10
+
+console.log(num1)
+console.log(num2)
+console.log(num3)
+*/ 
---
+++ aula4/parte2/aula4+.js
@@ -0,0 +1,86 @@
+let num1=0, num2=0, res=0
+
+num1=10
+num1/=2
+ 
+console.log(num1)
+/* agora o foco é operadores: 
+ As operacoes podemos trabalhar com aritmeticos, exemplo res= num1 + num2 , atribui  num1=5 , num2=10
+
+Posso fazer assim : console.log(num2-num1)
+res=(num1+num2)*2
+
+console.log(num2-num1)
+
+quando usa ( )tem preferencia em todo operadores! ele faz soma e depois multiplicação.
+
+res=(num1+num2)*2
+
+console.log(res)
+
+se eu tiro () a multiplicação vai primeiro!
+
+let num1=0, num2=0, res=0
+
+num1=5
+num2=15
+
+res=num2/num1
+
+console.log(res)
+
+----------------------------------
+
+let num1=0, num2=0, res=0
+
+num1=2
+num2=15
+
+res=num2%num1
+
+console.log(num2/num1)
+console.log(num2%num1)
+
+
+----------------------------------------
+
+incremnte let num1=0, num2=0, res=0
+
+num1=10
+num1=++
+num1=++
+num1=++
+num1=++
+num1=++
+ 
+console.log(num1)
+
+---------------------------------------------
+decrimento : 
+let num1=0, num2=0, res=0
+
+num1=10
+num1=--
+num1=--
+num1=--
+num1=--
+num1=--
+ 
+console.log(num1)
+
+-----------------------------------------------
+
++=
+
+let num1=0, num2=0, res=0
+
+num1=10
+num1+=1
+ 
+console.log(num1)
+
+-------------------------------------------
+
+
+
+*/
