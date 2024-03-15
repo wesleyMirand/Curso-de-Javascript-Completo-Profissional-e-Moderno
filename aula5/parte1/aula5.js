b88466850a810d80e5ccb1e4dab5a1b7cf5f6b35
wesley Mirand <wewesantos@hotmail.com>
2024-03-15 11:43:11 -0300

    1

---
+++ aula5/parte1/aula5.js
@@ -0,0 +1,23 @@
+let num1= 10
+let num2= 5
+let num3= 10
+
+console.log(num1 != num3) 
+
+/* console.log(num1 > num2)  num1 é maior que 2 = true vdd
+console.log(num1 < num2) false 
+
+console.log(num1 >= num3)  true 
+
+console.log(num1 <= num3)  true 
+
+console.log(num1 == num3) true
+
+console.log(!(num1 == num3))  o "NOT" invert, negacao do true pro false
+
+console.log(num1 != num3)  num1 diferente de 3 ? nao! false
+
+console.log(!(num1 != num3)) true
+
+
+*/
