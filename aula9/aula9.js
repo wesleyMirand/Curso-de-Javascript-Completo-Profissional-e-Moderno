f2ffc7a7ea5f3c00ac88a63260bf25d1212071b3
wesley Mirand <wewesantos@hotmail.com>
2024-03-19 16:52:31 -0300

    1

---
+++ aula9/aula9.js
@@ -0,0 +1,95 @@
+let n1=10
+let n2=20
+
+console.log(n1 + "oi" + n2)
+/* 
+pre e pos-incremento:
+n++ incrementando em 1 n imprime 11
+let n=10
+
+n++
+
+console.log(n)
+, 
+assim vai da 10, incremento no momento que estou usando, usando e incrementando :
+let n=10
+
+console.log(n++)
+
+10 porque é opereacao de pois incremento, usa depois incrementa.
+
+++n pre, ele incremento o valor de n deu 11:
+let n=10
+
+console.log(++n) ,
+
+e agora os dois que vao da 11, 
+
+let n=10
+
+console.log(++n)
+console.log(n),
+
+assim vai da 10 e 11, pos e pre incremento:
+
+let n=10
+
+console.log(n++)
+console.log(n)
+
+Sempre vai utilizar primeiro o elemento da esquerda "N" e depois ++.
+++ N da ao contrario e incrementa.
+
+
+Assim vai da 20:
+let n=10
+
+n+=10
+
+console.log(n) .
+
+
+Decrimento 9:
+let n=10
+
+console.log(--n),
+
+incremento 10:
+let n=10
+
+console.log(n--),
+
+assim vai da -10:
+
+let n=10
+
+console.log(n) , 
+
+Operador inversao, -n inversao :
+let n=1
+let x=-n
+
+console.log(x), 
+assim 10  normal invertendo o -10, 10.
+let n=-10
+let x=-n
+
+console.log(x).
+
+Concatenacao vai da 30: 
+let n1=10
+let n2=20
+
+console.log(n1 + n2),
+
+Porém se eu tiver uma string no meio, ele  considera que o + vira concatenacao e inves de
+somar ele vai juntar 1020.
+
+let n1=10
+let n2=20
+
+console.log(n1 + "" + n2)
+
+
+
+*/
