dc1e3692ae74539b280d1998125c764ca7b1cfd9
wesley Mirand <wewesantos@hotmail.com>
2024-03-19 19:31:31 -0300

    3

---
+++ typeof/aula11/aula11.js
@@ -0,0 +1,28 @@
+let v1=10
+let v2="10"
+let v3=v1===v2
+let v4={nome:"Pedro"}
+
+console.log("Valor:" + v1 + " - Tipo:" + typeof(v1))
+console.log("Valor:" + v2 + " - Tipo:" + typeof(v2))
+console.log("Valor:" + v3 + " - Tipo:" + typeof(v3))
+console.log("Valor:" + v4 + " - Tipo:" + typeof(v4)) 
+/*typeof dentro o valor da var que quer verificar: typeof(v2))
+"valor  " concatena com o valor da variavel  + v1 + e " - tipo " + typ... escrever o
+valor da var
+
+qualquer valor numerico ele considera como tipo Number. Operador UNARIO retorna o 
+tipo da var */
+
+/*
+tipo do operando, tipo da var ou do elemento: 
+
+comparacao
+let v1=10
+let v2="10" era pra ser texto
+let v3=v1===v2  valor igualdade tipo diferente
+let v4={nome:"Bruno"} valor objeto inteiro
+
+retorna o tipo de dado da var.
+
+*/
