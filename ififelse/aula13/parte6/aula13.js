d6fdda92f78d4b8812a62336e2bede9300b7ac5a
wesley Mirand <wewesantos@hotmail.com>
2024-03-21 15:00:58 -0300

    6

---
+++ ififelse/aula13/parte6/aula13.js
@@ -0,0 +1,45 @@
+let num=10
+
+if(num > 10) {
+   console.log("Numeral mamior que  10")
+}else if(num > 5){
+   console.log("Numeral entre 6 a 10")
+}else{
+   console.log("Numeral menor ou igual que 5")
+}
+console.log("Fim do programa") 
+
+/* 
+
+Posso ter varias condicao dentro do if, nao precisa ser so
+if else, posso encher de else.
+Ou alinhar os if que é um if dentro de outro.
+
+Outro Teste: 
+
+
+let num=100
+
+"Se numeral for maior que 10"
+if(num > 10) {
+
+   "ele cai aqui sempre que for maior que 10"
+   console.log("Numeral mamior que  10")
+
+
+   "se nao for maior que 10 e ser maior que 5"
+}else if(num > 5) {
+ 
+   "cai aqui"
+console.log("Numeral esta entre 6 a 10")
+
+   "caso o contrario"
+}else 
+{
+
+   "caia qui"
+   console.log("Numeral menor ou igual a 5")
+}
+console.log("Fim do programa")
+
+*/
