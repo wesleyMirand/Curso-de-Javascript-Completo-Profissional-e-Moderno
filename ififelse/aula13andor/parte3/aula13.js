2ade3dc6cc496bf2878a5538ba5783e2bbff8dce
wesley Mirand <wewesantos@hotmail.com>
2024-03-21 15:42:32 -0300

    8

---
+++ ififelse/aula13andor/parte3/aula13.js
@@ -0,0 +1,10 @@
+let energia=100
+let malhar="aberta"
+
+if( energia > 60 && malhar=="aberta") {
+   console.log("vou pra cademia")
+}else {
+   console.log("Vou ficar em casa")
+}
+
+console.log("fim programa")
