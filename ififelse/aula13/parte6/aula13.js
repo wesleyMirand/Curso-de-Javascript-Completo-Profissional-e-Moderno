55b2192c457d382346150d5ad1c6d07c9df31d5e
wesley Mirand <wewesantos@hotmail.com>
2024-03-21 15:34:13 -0300

    7

--- ififelse/aula13/parte6/aula13.js
+++ ififelse/aula13/parte6/aula13.js
@@ -1,7 +1,10 @@
-let num=10
+let num=100
 
 if(num > 10) {
-   console.log("Numeral mamior que  10")
+   console.log("Numeral maior que  10")
+   if(num > 50){
+      console.log("numeral é maior que 50")
+   }
 }else if(num > 5){
    console.log("Numeral entre 6 a 10")
 }else{
@@ -10,36 +13,12 @@ if(num > 10) {
 console.log("Fim do programa") 
 
 /* 
+Usei 1 if dentro do proprio if, e o programa vai me retorna
+os dois comando porque os dois sao verdadeiro.
+Quero saber se é maior que 10 , mais quero verifica se num 
+é maior que 50 tambem.
 
-Posso ter varias condicao dentro do if, nao precisa ser so
-if else, posso encher de else.
-Ou alinhar os if que é um if dentro de outro.
+E se eu por 40 vai executar só o primeiro if.
 
-Outro Teste: 
-
-
-let num=100
-
-"Se numeral for maior que 10"
-if(num > 10) {
-
-   "ele cai aqui sempre que for maior que 10"
-   console.log("Numeral mamior que  10")
-
-
-   "se nao for maior que 10 e ser maior que 5"
-}else if(num > 5) {
- 
-   "cai aqui"
-console.log("Numeral esta entre 6 a 10")
-
-   "caso o contrario"
-}else 
-{
-
-   "caia qui"
-   console.log("Numeral menor ou igual a 5")
-}
-console.log("Fim do programa")
 
 */
