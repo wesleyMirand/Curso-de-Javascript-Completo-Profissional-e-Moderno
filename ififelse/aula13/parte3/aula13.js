9314d24ba21a78f7eaedbec778daaee529e0117a
wesley Mirand <wewesantos@hotmail.com>
2024-03-21 14:36:49 -0300

    3

--- ififelse/aula13/parte3/aula13.js
+++ ififelse/aula13/parte3/aula13.js
@@ -1,11 +1,28 @@
 let num=100
 
-if(num > 10) 
+if(num > 10) {
    console.log("Numeral maior que  10")
-
+   console.log("comando 2")
+}
 console.log("fim do programa")
 
 
 /* 
-Quando o if possui somente um comando, ao 
+Nesse caso é obrigado a usar {} chaves, porque tem dois comando.
+
+se eu fizer sem a chave:
+let num=10
+
+if(num > 10) 
+   console.log("Numeral maior que  10")
+   console.log("comando 2")
+
+console.log("fim do programa")
+
+ele vai retorna  :
+comando 2
+fim do programa
+
+ele entendeu que o comando2 nao faz parte do bloco if!
+Resumindo o escopo é definido pela chave de blocos!
 */
