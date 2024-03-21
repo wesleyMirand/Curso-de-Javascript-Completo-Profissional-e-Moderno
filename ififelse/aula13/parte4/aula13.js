f9fe6b26fa1ec5334d2a6ab2ab32632d3351e687
wesley Mirand <wewesantos@hotmail.com>
2024-03-21 14:49:00 -0300

    5

---
+++ ififelse/aula13/parte4/aula13.js
@@ -0,0 +1,33 @@
+let num=10
+
+if(num > 10) {
+   console.log("Numeral mamior que  10")
+}else{
+   console.log("Numeral menor ou igual que 10")
+}
+console.log("Fim do programa") 
+
+/* 
+
+Se eu quiser executar, algo quando for false vou usar o else .
+O else é um caso contrario! Um SE NAO!
+
+let num=10
+"se numero for maior que 10"
+if(num > 10) { 
+
+   "executa esse bloco"
+   console.log("Numeral mamior que  10")
+
+"caso o contrario"
+}else{ 
+
+"executa esse bloco"
+   console.log("Numeral menor ou igual que 10")
+}
+
+console.log("Fim do programa") 
+
+Se num for maior que 10 ele execu
+
+*/
