641da134ad13a3889fbac3390dbfb941850a5bfa
wesley Mirand <wewesantos@hotmail.com>
2024-03-22 18:22:45 -0300

    5

---
+++ for/parte3cuidados/aula15.js
@@ -0,0 +1,15 @@
+console.log("Inicio do programa")
+
+for(let i=0; i >= 0; i++) {
+     console.log("CFB Cursos - valor do i: " + i)
+}
+
+console.log("Fim do programa")
+
+/*
+
+cuidados: Pra nao cair no loop infinito, se colocar i começa em 0 "i >=10"for maior ou
+igual a zero e i++.
+Ao executar vai cair na rotina de loop infinito, nunca acaba.
+
+*/
