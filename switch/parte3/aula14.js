e1357a19f0e1d172cb6c56c0a0d2bc8f927d79e8
wesley Mirand <wewesantos@hotmail.com>
2024-03-22 16:25:22 -0300

    3

---
+++ switch/parte3/aula14.js
@@ -0,0 +1,30 @@
+let colocacao = 10
+
+switch(colocacao){
+      case 1:
+            console.log("Primeiro Lugar")
+            console.log("Foi o melhor")
+            console.log("Classificado para a final")
+            break
+      case 2:
+            console.log("Segundo Lugar")
+            break
+      case 3:
+            console.log("3lugar")
+            break
+      case 4: case 5: case 6:
+            console.log("Premio de participacao")
+            break
+      default:
+            console.log("Nao subiu ao podio")
+            break
+}
+
+
+/*
+
+Eu quero avalia, case4, case5, case6. Caso 4 5 ou 6: (premio de participacao).
+se for 7 vai pro default.
+
+
+*/ 
