bab384c5b9dac996af62f1aa3bd5dc977fb775dd
wesley Mirand <wewesantos@hotmail.com>
2024-03-22 18:03:30 -0300

    4

---
+++ for/parte1/aula15.js
@@ -0,0 +1,64 @@
+console.log("Inicio do programa")
+
+for(let i=0; i < 10; i++) {
+     console.log("CFB Cursos")
+}
+
+console.log("Fim do programa")
+
+
+/*
+
+For: 
+FOR-> PARA , preciso de 3 configuracao: 1Iniciação, 2Condição(oque avalia se vou continuar ou nao),
+3Controle(incremento ou decremento, diminui aumenta ) E dentro da chaves o bloco de comando.
+Se tiver só um comando a chave nao é obrigatoria, igual o if.
+
+Quero repetir um bloco de comandos, com sintaxe do for, iniciacao var de controle,
+chamar o let com "i" de indice e inicia ela com zero "let i=0;". e o segundo é a 
+condicao de execução enquanto i for menor que 10 "i < 10;"Ja sabendo que vou interar
+o comando 0 a 9 vezes até chegar em 10. Ai a cada interação vou interar o controle em
+1 com "i++" de incremento.  "i++ pode ser : i = i+1, i++, i+=2". Nesse caso estou
+aumentando a cada interação o i em 1. 
+
+0-1-2-3-4-5-6-7-8-9-10 é continua, i é menor que 10 não! ele é igual a 10!
+até que : i<10 retorna falso e sai nao interando mais o looping.
+E continua a execução normal do programa.
+
+Resumindo o for vai ser executado enquanto a sua condicao, for vdd.
+
+console.log("Inicio do programa")
+
+for(let i=0; i < 10; i++) {
+     console.log("CFB Cursos")
+}
+
+console.log("Fim do programa")
+
+Executo inicio do programa, fico preso no loop intero 10 rodando 10vezes, não é mais falso
+sai do loop e segue a rotina normal do programa.
+
+Ele começa i inicia 0, i é menor que 10 ? SIM VDD.incremento 1 e executo o comando, 
+ai testo de novo e nao vou usar mais o "i=0" porque ele é so pra iniciar do incremento
+E logo em seguida usar chaves "{}" com o bloco de comando dentro.
+Ai testo de novo i é menor que 10? sim ai continuo até nao ser mais e chegar no 10.
+
+Estrutura em loopig vai repetir uma bloco de comandos ou um só.Sempre que eu precisar
+repetir uma intrução eu utilizo um looping, que é interacoes. Chamar as repeticoes de
+interacoes, cada repeticao executada e uma interacao uma sequencia, comando, bloco...
+Definidos e indefinidos, loop eu vou usar quando eu saber quantas vezes eu vou querer
+repetir, tenho nocao em quantas vezes vou interar os comandos. E os indefinidos, quando
+nao sei ao certo quantas vezes vou interar o bloco de comandos...
+
+     |> 1DEFINIDOS ->  FOR
+     |
+LOOPS|
+     |                 |> whiLE
+     |> 2INDEFINIDOS-> 
+                       |> Do While
+
+Qual a diferenca do FOR para o WHILE? 
+Utilizamos o for quando tenho nocao das vezes que preciso interar o bloco de comandos.
+Quando nao souber usar o While ou Do While.
+
+*/
