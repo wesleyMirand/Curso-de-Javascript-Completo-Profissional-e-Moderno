ad0701b268de4da0849efaf610f082e4c50adcc9
wesley Mirand <wewesantos@hotmail.com>
2024-03-23 23:59:50 -0300

    1

---
+++ for/parte6/aula15.js
@@ -0,0 +1,9 @@
+console.log("Chances de pular de paraquerda")
+for(let i=0; i<350; i++) {
+     if(i%2 == 0){
+          console.log(i +"tente outra vez")
+     }else{
+          console.log(i + "tente")
+     }
+}
+console.log("pulou")
