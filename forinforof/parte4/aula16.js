b468a3063059b60abc118b35bf39d916b11b83b0
wesley Mirand <wewesantos@hotmail.com>
2024-04-02 23:57:22 -0300

    1

--- forinforof/parte4/aula16.js
+++ forinforof/parte4/aula16.js
@@ -1,16 +1,35 @@
 let num=[10, 20, 30, 40, 50]
 
+for(n of num) {
+      console.log(n)
+}
+
 for ( n in num) {
       console.log(n)
 }
 
-/*for(let i=0; i<num.length;i++) {
+/* for(let i=0; i<num.length;i++) {
       console.log(num[i])
-} */
+ */
 
 /* Existem outros 2 for, o for in, vou criar uma var que vai receber os elementos, 
 retornados da posição que quero trabalhar, for (n in num ){ }, vou interar a posição num,
 e vou percorrer e retorna os elementos.
 Em cada posicao vai colocar o num, console.log(i). vou imprimir 0 ao 4, esse for e igual
 o primeiro for. 
+
+Por padrao no n percorre todos elementos.
+
+os dois lop sao iguais e tem o mesmo retorno, mais usei o for of que vai dentro elemento,
+ da
+condicao que ele esta interando: 
+for ( n in num) {
+      console.log(n)
+}
+
+ for(let i=0; i<num.length;i++) {
+      console.log(num[i])
+
+
+
 */
---
+++ loopWhile/parte1/aula17.js
@@ -0,0 +1,18 @@
+
+
+/*loop while: indefinido, enquanto.
+ele utiliza quando nao tenho certeza das vezes que vou usar os comando, ai tem regras.
+Como funciona o DIAGRAMA:
+                          
+               inicio    *
+                         |
+                         |                               FOR FALSA
+                    |-==>| CONDICAO   | condicional while --------------------------
+        DEPOIS      |         |                                                     |
+          QUE       |         |<enquanto for verdadeira, ele vai executa o bloco    |
+         EXECUT     --->|BLOCO         |                                            |
+    ELE VOLTA PRA CIMA                                                              |
+                                                                                   |
+                                                          | CONTINUACAO EXEC NORMAL |
+
+    */                                                                           
