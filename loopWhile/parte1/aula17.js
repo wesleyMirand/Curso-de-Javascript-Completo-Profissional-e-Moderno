2196cdaf9a9540077e5adc546cb9de5b3e8466ca
wesley Mirand <wewesantos@hotmail.com>
2024-04-05 22:01:13 -0300

    1

--- loopWhile/parte1/aula17.js
+++ loopWhile/parte1/aula17.js
@@ -1,13 +1,25 @@
-let n=0
+let n=10
 
 while(n<10) {
-      console.log(n)
+      console.log("CfB Cursos")
+      n++
 }
+console.log("fim do programa")
 
 
 
 
 /*
+
+while primeiro teste de comando
+se retorna true executo bloco se nao nao executo
+nao tenho ctza se vai ser executado de uma vez
+
+do while teste de comando  , DOWHILE tem a garantia que pelo menos uma vez 
+o bloco vai ser executado.
+executa e depois do primeiro teste ele executa
+
+s
 
 loop while: indefinido, enquanto.
 ele utiliza quando nao tenho certeza das vezes que vou usar os comando, ai tem regras.
---
+++ loopWhile/parte1/parte2/aula17.js
@@ -0,0 +1,9 @@
+let n=10
+
+do(n<10) {
+      console.log("CfB Cursos")
+      n++
+}while(n<10)
+console.log("fim do programa")
+
+//executa e depois verifica a condicao. n++ incrementa para nao ter loop infinito
