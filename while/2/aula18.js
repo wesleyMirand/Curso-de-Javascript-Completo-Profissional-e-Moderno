f2b6bda0b4ccc5f04e895606bac0c5bd275a18c2
wesley Mirand <wewesantos@hotmail.com>
2024-04-09 23:34:12 -0300

    2

--- while/2/aula18.js
+++ while/2/aula18.js
@@ -1,32 +1,6 @@
 let n=10
-while(n<10){
+do{ //faca executa esse comando e depois faca o teste
       console.log("WEWE CURSOS")
-      n++ //n é maior do que 10? nao false! vai direto pro fim do do programa
-      
-}
+      n++ 
+}while(n<10) //teste no final, loop interado pelo menos uma vez
 console.log("Fim")
-
-
-
-/* 
-while
-e 
-dowhile
-ambos vao executar enquanto for vdd.
-Ambos sao lops para usar quando nao souber a quantidade de vez.
- 
-while -> expressao -> enquanto e verdadeira eu executo
-Primeito testo
-se retorna true executo bloco
-retorna false nao executo
-NAO TENHO GARANTIA QUE O BLOCO VAI SER EXECUTADO, se começa falso ele ja nem começa.
-
-dowhile -> começo com DO"FACA", -> bloco de comandos -> final do teste executo o while(expressao logica)
-Primeiro executa e depois vai testa! com a expressao logica.
-ele começa mesmo que for falso, ai ele executa e sai do lopp e continua o programa.
-
-
-NO DOWHILE ele executa pelo menos uma vez
-NO WHILE NAO tenho garantia!
-
-*/
