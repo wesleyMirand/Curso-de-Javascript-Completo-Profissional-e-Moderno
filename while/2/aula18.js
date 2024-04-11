c33047f4c633d1ef380209b82d1e57b61c0c8449
wesley Mirand <wewesantos@hotmail.com>
2024-04-10 23:59:20 -0300

    1

--- while/2/aula18.js
+++ while/2/aula18.js
@@ -1,6 +1,6 @@
 let n=10
 do{ //faca executa esse comando e depois faca o teste
       console.log("WEWE CURSOS")
-      n++ 
+      n++ //sempre implementa var pra nao se torna um loop infinito
 }while(n<10) //teste no final, loop interado pelo menos uma vez
 console.log("Fim")
