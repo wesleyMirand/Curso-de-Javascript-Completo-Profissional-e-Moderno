9085601b3e15f749f7ffc982eac4d407567cb582
wesley Mirand <wewesantos@hotmail.com>
2024-04-11 23:51:44 -0300

    2

---
+++ break/parte2/aula20.js
@@ -0,0 +1,24 @@
+let n = 0 
+let max = 1000
+
+
+while(n<max) {
+      console.log("Wewe curso" + n) //n = 0
+      if(n >10) {
+            break
+      }
+      n++
+}
+console.log('fim')
+
+
+/* 
+se no meio ter execucao, se n for maior que 10 uso o break, valendo 9 incrementa 1.
+n e 10 
+10 é menor que mil continua
+n maior que 10 ainda nao?
+11 menor que mil
+11 maior que 10
+encontra o break encerra e faz o ultimo comando fim
+
+*/
