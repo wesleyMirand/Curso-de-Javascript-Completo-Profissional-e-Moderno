3364d9cc176301c86f4c0b711f15d843bc4c6457
wesley Mirand <wewesantos@hotmail.com>
2024-04-11 23:48:21 -0300

    1

---
+++ break/parte1/aula20.js
@@ -0,0 +1,18 @@
+let n = 0 
+let max = 1000
+
+
+while(n<max) {
+      console.log("Wewe curso")
+      n++
+}
+console.log('fim')
+
+
+/* 
+break e continue: o Break é um expressao que interrome a execução. Para o loop encerra
+
+continue: diferente ele para somente naquela interacao e pula pra outra, sai da atual
+e vai pra proxima, até encontra outro continue. 
+
+*/
