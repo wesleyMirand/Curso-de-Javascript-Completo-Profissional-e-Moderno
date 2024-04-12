313303a11508c0183524257e2860d067fd82e4d6
wesley Mirand <wewesantos@hotmail.com>
2024-04-12 00:11:37 -0300

    5

---
+++ break/parte4/aula20.js
@@ -0,0 +1,23 @@
+let n = 0 
+let max = 1000
+
+
+for(let i=n; i<max; i++) { 
+      console.log("Wewe curso" + i)
+      if(i%2==0){
+            pares++
+      }
+}
+
+console.log("quantidade de pares: "+pares)
+console.log('fim')
+
+
+/* 
+
+se eu encotnra um continue, afirmo que ele executo tal vezes.
+quantos numeros pares executo até o mil
+se i mood 2 for igual a 0 , mood par.
+quantidades de pares
+
+*/
