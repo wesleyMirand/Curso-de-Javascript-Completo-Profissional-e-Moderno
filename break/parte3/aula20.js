1e9cbae84340420aff2edfe022f7dcf12bd4b783
wesley Mirand <wewesantos@hotmail.com>
2024-04-12 00:05:26 -0300

    4

--- break/parte3/aula20.js
+++ break/parte3/aula20.js
@@ -2,23 +2,18 @@ let n = 0
 let max = 1000
 
 
-while(n<max) {
-      console.log("Wewe curso" + n) //n = 0
-      if(n >10) { //verifica a condicao que o break quer parar o lop vdd parou false continue
-            break
-      }
-      n++
+for(let i=n; i<max; i++) { 
+      console.log("Wewe curso" + i)
 }
+
 console.log('fim')
 
 
 /* 
-se no meio ter execucao, se n for maior que 10 uso o break, valendo 9 incrementa 1.
-n e 10 
-10 é menor que mil continua
-n maior que 10 ainda nao?
-11 menor que mil
-11 maior que 10
-encontra o break encerra e faz o ultimo comando fim
+
+let i igual a n, enquanto n for menor que max vou incrmenta o i++
+consoele com o valor da var i
+e fim do programa
+vai executa mil vezes
 
 */
