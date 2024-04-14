e8ed669743f0ab74edfdc4877d949c50a39b0b74
wesley Mirand <wewesantos@hotmail.com>
2024-04-13 22:29:42 -0300

    8

--- funcoes/parte6/aula21.js
+++ funcoes/parte6/aula21.js
@@ -8,22 +8,12 @@ d2.innerHTML("wewecurso")
 d3.innerHTML("wewecurso") //ja esta executando.
 }
 
-for(let i=0; i<10; i++){
-   nome()
-}
+mudarTexto()
 
 /* 
 
-camecase snapcase quando tem _.
-Vou pegar as div
-let d1 = document.getlemenbyid("d1")
-let d2 = document.getlemenbyid("d1")
-let d3 = document.getlemenbyid("d1")
-d1.innerHTML("wewecurso")
-d2.innerHTML("wewecurso")
-d3.innerHTML("wewecurso")
+chamei a funcao, executa e no momento faz alteracao do conteudo.alteracao.
+vou criar um button;
 
-manipular o dom com inner.html como texto ("wewecursos")
-tenque usar o DOM no browser.
 
 */
