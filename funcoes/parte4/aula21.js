5972b4061954c81ac48faa43be969c6c533225da
wesley Mirand <wewesantos@hotmail.com>
2024-04-13 21:54:58 -0300

    3

---
+++ funcoes/parte4/aula21.js
@@ -0,0 +1,17 @@
+function nome() { // Declaracao da Funcao, quando crio a funcao, nao preciso executa, preparo ela pra quando precisar!
+      console.log("WeWe Cursos")
+}
+
+nome()         //chamada da funcao
+nome()
+nome()
+nome()
+nome()
+nome()
+
+/* 
+
+posso por dentro um loop:
+for, let igual a 0, igual i for menor que 10 , i++, 
+
+*/
