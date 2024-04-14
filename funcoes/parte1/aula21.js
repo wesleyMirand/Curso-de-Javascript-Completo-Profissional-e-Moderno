3040e86062d382b3adb3fad8a40f501ba197f40d
wesley Mirand <wewesantos@hotmail.com>
2024-04-13 21:43:12 -0300

    1

---
+++ funcoes/parte1/aula21.js
@@ -0,0 +1,30 @@
+function nome() {
+      console.log("WeWe Cursos")
+}
+
+/* 
+
+funcoes: oque é ? é um bloco de comando. executar quando eu quiser no programa.
+ao longo do programa, se tenho funcoes, linha de codigo a execução é sequencial:
+1_ executa esse
+2_ esse
+3_ esee
+4_ esse........
+
+Agora se tenho um bloco com varios comando e do um nome pro bloco, só vou usar ele
+quando eu precisar! 
+1___________
+2___________
+3___________
+Deixo ele armazenado na memória ou qualquer outro lugar, fica guardado. E quando precisar
+chamo ele onde ele esta e levo pra executar no programa.
+Executo quando vezes eu precisar!
+
+Quando comecar?
+function, nome da funcao, () parenteses: funcao parametrizada e quando nao quiser passar
+parametro, iremos abrir e fechar () com nada dentro, nenhum parametro dentro da funcao.
+console.log("cfb curso").
+nao vai aparecer nada.
+ 
+
+*/
