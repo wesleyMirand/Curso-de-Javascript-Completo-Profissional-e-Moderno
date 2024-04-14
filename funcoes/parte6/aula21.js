3a63c27846d98bb995a49d923f783970af05c5e1
wesley Mirand <wewesantos@hotmail.com>
2024-04-13 22:22:38 -0300

    !

---
+++ funcoes/parte6/aula21.js
@@ -0,0 +1,29 @@
+function mudarTexto() { // Declaracao da Funcao, quando crio a funcao, nao preciso executa, preparo ela pra quando precisar!
+
+   let d1 = document.getlemenbyid("d1")
+let d2 = document.getlemenbyid("d2")
+let d3 = document.getlemenbyid("d3")
+d1.innerHTML("wewecurso")
+d2.innerHTML("wewecurso")
+d3.innerHTML("wewecurso") //ja esta executando.
+}
+
+for(let i=0; i<10; i++){
+   nome()
+}
+
+/* 
+
+camecase snapcase quando tem _.
+Vou pegar as div
+let d1 = document.getlemenbyid("d1")
+let d2 = document.getlemenbyid("d1")
+let d3 = document.getlemenbyid("d1")
+d1.innerHTML("wewecurso")
+d2.innerHTML("wewecurso")
+d3.innerHTML("wewecurso")
+
+manipular o dom com inner.html como texto ("wewecursos")
+tenque usar o DOM no browser.
+
+*/
