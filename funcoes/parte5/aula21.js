5185480ee2f456068a4e32c75d808c261274f3e2
wesley Mirand <wewesantos@hotmail.com>
2024-04-13 22:08:41 -0300

    5

--- funcoes/parte5/aula21.js
+++ funcoes/parte5/aula21.js
@@ -1,5 +1,8 @@
 function nome() { // Declaracao da Funcao, quando crio a funcao, nao preciso executa, preparo ela pra quando precisar!
-      console.log("WeWe Cursos")
+   let n1 = 2
+   let n2 = 10
+   let soma = n1 + n2   
+   console.log("soma")
 }
 
 for(let i=0; i<10; i++){
@@ -8,14 +11,18 @@ for(let i=0; i<10; i++){
 
 /* 
 
-posso por dentro um loop:
-for, let i igual a 0, enquanto i for menor que 10 , i++:
-for(let i=0; i<10; i++){
-   nome()
-}
- vou chamar a funcao nome 10 vezes dentro. 
+Uma funcao que realize calculo, soma 2 _ 10, vou operar do jeito que quiser
+n1= 2 n2= 10 let soma igual a n1 + n2
+e no final console.log(some)!
+soma2_10() <- quando vezes eu quiser chamar vai ser executada! Porque esta dentro
+de um looping!
+Eu criei um codigo, repeti os comando varias vezes :
+ let n1 = 2
+   let n2 = 10
+   let soma = n1 + n2   
+   console.log("soma")
+criei um loop!
 
- Simples com so um comando do jeito que eu quiser.
-
+ou  em momento espefico coloco dentro de uma funcao!
 
 */
