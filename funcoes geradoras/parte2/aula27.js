8262f10de2d50b3354598fc3b544d37c16ac7525
wesley Mirand <wewesantos@hotmail.com>
2024-04-27 02:56:43 -0300

    1

---
+++ funcoes geradoras/parte2/aula27.js
@@ -0,0 +1,17 @@
+function* cores(){
+      yield 'Vermelho'
+      yield 'Verde'
+      yield 'Azul'
+      
+}
+
+const itc = cores()
+console.log(itc)
+
+/* 
+
+Chamar funcao , criar const  var, 
+itc= cores()
+vai da em nd ! so foi retornado um funcao geradora
+
+*/
