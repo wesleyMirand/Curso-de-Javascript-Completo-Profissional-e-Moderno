f6312b5255aaefea5def723df2bf8764878eb4c2
wesley Mirand <wewesantos@hotmail.com>
2024-05-06 23:42:49 -0300

    1

---
+++ Metodo Map/parte2/aula28.js
@@ -0,0 +1,16 @@
+const cursos = ['HTML', 'CSS', 'JavaScript', 'PHP', 'React']
+
+let c=cursos.map((el, i) => {
+      return el
+})
+
+console.log(c)
+
+
+
+/* 
+
+o for usa quando percorre colecao, posso parar no meio. Quando quer percorrer inteira, trabalha elementos e retorna sem interrupcao.
+usar o map!
+retorna element com return el , cria var c = , com retorno map. console.log em c
+*/
