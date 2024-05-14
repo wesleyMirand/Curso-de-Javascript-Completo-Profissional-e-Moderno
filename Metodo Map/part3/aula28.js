087b13771a817920fa86034217e750630b21670b
wesley Mirand <wewesantos@hotmail.com>
2024-05-13 23:40:08 -0300

    1

---
+++ Metodo Map/part3/aula28.js
@@ -0,0 +1,24 @@
+let el= document.getElementsByTagName("div")
+
+el=[...el]
+console.log(el)
+
+el.map((e, i) => {
+      e.innerHTML =  "CFB CURSOS"
+})
+
+
+/* const cursos = ['HTML', 'CSS', 'JavaScript', 'PHP', 'React'ssfffffgflhjhf
+
+let c=cursos.map((el, i) => {
+      return "<div>" + el + "</div>"
+})
+
+console.log(c)
+
+
+
+o for usa quando percorre colecao, posso parar no meio. Quando quer percorrer inteira, trabalha elementos e retorna sem interrupcao.
+usar o map!
+retorna element com return el , cria var c = , com retorno map. console.log em c
+*/
