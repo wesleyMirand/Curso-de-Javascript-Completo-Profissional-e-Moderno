e47a0d29b89332441c113a1b0d75ad319468c657
WesleyMirand <wewesantos@hotmail.com>
2024-05-22 19:49:23 -0300

    9

--- addEventListener/parte2/aula34.js
+++ addEventListener/parte2/aula34.js
@@ -5,5 +5,5 @@ function msg(){
 /* 
 addEventos:Existem muitas forma de trabalhar com eventos em "HTML" e "JS", pode programar os eventos
 nas "TAG" ou diretamente nos arquivo "JS".
-A melhor forma é no js. Ao clicar vai chamar a funcao clicou 
+A melhor forma é no js.
 */
