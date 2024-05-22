05a89e25b3444b25f0b29eec95ed8273f8c258bf
WesleyMirand <wewesantos@hotmail.com>
2024-05-22 20:02:36 -0300

    11

--- addEventListener/parte3/aula34.js
+++ addEventListener/parte3/aula34.js
@@ -1,20 +1,15 @@
 const c1 = document.querySelector("#c1")
 
-const msg = () => {
+c1.addEventListener("click", () => { 
   alert("clicou")
-}
-
-c1.addEventListener("click", msg)
+})
 
 /* 
 
-Const o elemento que quero usar,  chama de c1, document, metodos pegar elementos , elemento c1
-const c1 = document.getElementById("c1") 
+Posso desparar chamar um funcao anonima arrowfucntion, programar direto oque vai ser executado!
+c1.addEventListener("click", () => { 
+  alert("clicou")
+})
 
-ou usar o queryselector e chamar o id #c1
-const c1 = document.querySelector("#c1")
-Os dois tanto faz.
-Obtive o elemento, que quero manipular e vou adicionar um elemento a esse evento. C1.  para add event usar addeventlistener,
-proprio para tratar eventos escutador de eventos. Vai receber evento de click, e oque vai se esperado funcao msg e nao precisa por "";
-a não ser que tenha entrada de argumento.
+
 */
---
+++ addEventListener/parte5/aula34.js
@@ -0,0 +1,15 @@
+
+
+/* 
+
+O function :
+function msg(){
+  alert("clicou")
+}
+
+Pode ser também um arrowfuntion, const msg recebe essa arrow.
+const msg = () => {
+  alert("clicou")
+}
+
+*/
