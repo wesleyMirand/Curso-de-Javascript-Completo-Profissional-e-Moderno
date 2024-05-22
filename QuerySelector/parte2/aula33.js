e9227b13967df28ae55ac652a0886da899782ffa
WesleyMirand <wewesantos@hotmail.com>
2024-05-22 14:52:50 -0300

    2

---
+++ QuerySelector/parte2/aula33.js
@@ -0,0 +1,27 @@
+const divTodas = [...document.getElementsByTagName("div")]
+const cursosTodos = [...document.getElementsByClassName("curso")]
+const cursosC1 = [...document.getElementsByClassName("c1")]
+const cursosC2 = [...document.getElementsByClassName("c2")]
+const cursoEspecial = document.getElementsById("c1")
+
+const query_divTodas = document.querySelector("div")
+console.log(divTodas)
+
+/*
+console.log(divTodas)
+console.log(cursosTodos)
+console.log(cursosC1)
+console.log(cursosC2)
+console.log(cursoEspecial)
+
+cursosC1.map((el) => {
+      el.classList.add("destaque")
+})
+
+Agora vamos começar usar o const chamando o query_divTodas.
+const query_divTodas = document.querySelector("div")
+console.log(divTodas)
+O querydiv Todas, eu peguei somente 1 div de todos elementos, que são div retornam o primeiro elemento,
+que encontra da chave especifica.Achou a primeira div da pagina ele vai retorna.
+
+*/
