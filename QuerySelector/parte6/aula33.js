2f6daa0dfe624854db90cba71b43b5566af346ac
WesleyMirand <wewesantos@hotmail.com>
2024-05-22 18:27:02 -0300

    4

--- QuerySelector/parte6/aula33.js
+++ QuerySelector/parte6/aula33.js
@@ -8,13 +8,14 @@ const query_divTodas = [...document.querySelectorAll("div")]
 const query_cursosTodos = [...document.querySelectorAll(".curso")]
 const query_cursosC1 = [...document.querySelectorAll(".c1")]
 const query_cursosC2 = [...document.querySelectorAll(".c2")]
-const query_cursoEspecial = document.getElementsById("#c1")
+//const query_cursoEspecial = document.querySelector("#c1")
+const query_cursoEspecial = document.querySelectorAll("#c1")[0]
 
 console.log(query_cursosEspecial)
 //console.log(query_cursosC2)
 
 /*
 
-E tbm posso usar o querySelector com id assim : #c1. retorna so 1
-
+agora o query SelectorALL. retorna um node list
+colocar [0] especificando que quero um elemento da posição 0.
 */
--- QuerySelector/parte7/aula33.js
+++ QuerySelector/parte7/aula33.js
@@ -4,17 +4,21 @@ const cursosC1 = [...document.getElementsByClassName("c1")]
 const cursosC2 = [...document.getElementsByClassName("c2")]
 const cursoEspecial = document.getElementsById("c1")
 
-const query_divTodas = [...document.querySelectorAll("div")]
+const query_divTodas = [...document.querySelectorAll("div, p")]
 const query_cursosTodos = [...document.querySelectorAll(".curso")]
 const query_cursosC1 = [...document.querySelectorAll(".c1")]
 const query_cursosC2 = [...document.querySelectorAll(".c2")]
-const query_cursoEspecial = document.getElementsById("#c1")
+//const query_cursoEspecial = document.querySelector("#c1")
+const query_cursoEspecial = document.querySelectorAll("#c1")[0]
 
-console.log(query_cursosEspecial)
+console.log(query_divTodas)
 //console.log(query_cursosC2)
 
 /*
 
-E tbm posso usar o querySelector com id assim : #c1. retorna so 1
-
+Vamos usar agorao query_divTodas, com todos elementos da tag div e p.
+Mudar os elementos no html.
+ = [...document.querySelectorAll("div, p")] colocar o p.
+E agora voltou a ter 14 elementos. Especificou dois tipo de elementos diferente.
+E se quise especificar classe e div? pode!
 */
