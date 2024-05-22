0a895588a1d5b622d6e5853451e9716fe616736c
WesleyMirand <wewesantos@hotmail.com>
2024-05-22 19:43:25 -0300

    8

---
+++ addEventListener/parte1/aula34.js
@@ -0,0 +1,9 @@
+function msg(){
+  alert("clicou")
+}
+
+/* 
+addEventos:Existem muitas forma de trabalhar com eventos em "HTML" e "JS", pode programar os eventos
+nas "TAG" ou diretamente nos arquivo "JS".
+A melhor forma é no js.
+*/
