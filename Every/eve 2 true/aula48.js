fec3e148968c0d0ed1afc0d045a5d17c702b2a7e
WesleyMirand <wewesantos@hotmail.com>
2024-06-14 23:07:58 -0300

    1

---
+++ Every/eve 2 true/aula48.js
@@ -0,0 +1,31 @@
+const p_array = document.querySelector("#array")
+const btnVerificar = document.querySelector("#btnVerificar")
+const resultado = document.querySelector("#resultado")
+
+const elementos_array = [21, 25, 19, 20, 51, 18, 22]
+p_array.innerHTML =  "["+elementos_array+"]"
+
+btnVerificar.addEventListener("click",(evt) => {
+    resultado.innerHTML = "Array não conforme"
+    const ret=elementos_array.every((e,i) => {
+        return  e>=18
+    })
+    if(ret) {
+        resultado.innerHTML = "OK"
+    }
+    //console.log(ret)
+})
+
+
+
+
+
+
+/* 
+verificar o array e procura equivalencia, se todos elementos equivalrem a funcao
+e a regra.  se for array de idades, elemento por elemento e todos tenque valer
+a regra especifica, tipo 18+ maiores de 18. retorna true ok
+se noa false
+
+Para ser true todos tenque ser equivalente.
+*/
---
+++ Find/array letra/aula47.js
@@ -0,0 +1,29 @@
+const p_array = document.querySelector("#array")
+const text_pesquisar = document.querySelector("#txt_pesquisar")
+const btnPesquisar = document.querySelector("#btnPesquisar")
+const resultado = document.querySelector("#resultado")
+
+const elementos_array = ["html", "css", "javascript"]
+p_array.innerHTML =  "[" + elementos_array + "]"
+
+btnPesquisar.addEventListener("click" , (evt) => {
+    resultado.innerHTML = "Valor não encontrado"
+     const ret = elementos_array.find((e, i) => {
+        if (e==text_pesquisar.value){
+            resultado.innerHTML="Valor encontrado " + e + "na posição" + i
+            return e
+        }
+    })
+    console.log(ret)
+})
+ 
+
+/*
+tudo a mesma coisa : 
+const pes= (e, i) => {
+
+}
+
+function pesq(e, i ) {
+    
+ } */
