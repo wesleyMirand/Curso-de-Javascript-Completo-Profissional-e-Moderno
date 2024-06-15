b8c00be0a8bb261c10b1c2e8ba52d81120d2d7f8
WesleyMirand <wewesantos@hotmail.com>
2024-06-14 23:13:57 -0300

    2

--- Every/eve 1 false pt2/aula48.js
+++ Every/eve 1 false pt2/aula48.js
@@ -2,7 +2,7 @@ const p_array = document.querySelector("#array")
 const btnVerificar = document.querySelector("#btnVerificar")
 const resultado = document.querySelector("#resultado")
 
-const elementos_array = [21, 25, 19, 18, 51, 18, 22]
+const elementos_array = [21, 25, 19, 20, 51, 18, 22]
 p_array.innerHTML =  "["+elementos_array+"]"
 
 btnVerificar.addEventListener("click",(evt) => {
--- Every/eve 3 posicao/aula48.js
+++ Every/eve 3 posicao/aula48.js
@@ -2,12 +2,15 @@ const p_array = document.querySelector("#array")
 const btnVerificar = document.querySelector("#btnVerificar")
 const resultado = document.querySelector("#resultado")
 
-const elementos_array = [21, 25, 19, 18, 51, 18, 22]
+const elementos_array = [21, 25, 19, 16, 51, 18, 22]
 p_array.innerHTML =  "["+elementos_array+"]"
 
 btnVerificar.addEventListener("click",(evt) => {
-    resultado.innerHTML = "Array não conforme"
+    
     const ret=elementos_array.every((e,i) => {
+        if( idade < 18) {
+            resultado.innerHTML = "Array não conforme na posição do " + i
+        }
         return  e>=18
     })
     if(ret) {
@@ -28,4 +31,7 @@ a regra especifica, tipo 18+ maiores de 18. retorna true ok
 se noa false
 
 Para ser true todos tenque ser equivalente.
+
+compar todos elementos e retorna ok quando todos tiverem conforme regra, se 1 não
+ja retorna false. conformidade de array.
 */
