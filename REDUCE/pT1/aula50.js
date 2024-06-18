d89c778b9fd1cc51b4719b5bc9c6cb36aada8246
WesleyMirand <wewesantos@hotmail.com>
2024-06-18 12:15:01 -0300

    1

--- REDUCE/pT1/aula50.js
+++ REDUCE/pT1/aula50.js
@@ -2,28 +2,38 @@ const p_array = document.querySelector("#array")
 const btnReduzir = document.querySelector("#btnReduzir")
 const resultado = document.querySelector("#resultado")
 
-const elementos_array = [16, 12, 10, 17, 15, 13, 11, 19]
+const elementos_array = [1, 2, 3, 4, 5]
+let ant = []
+let atu = []
+let dobro = []
+
 p_array.innerHTML =  "["+elementos_array+"]"
 
-btnVerificar.addEventListener("click",(evt) => {
-    
-    const ret=elementos_array.some((e,i) => {
-        if( idade < 18) {
-            resultado.innerHTML = "Array não conforme na posição do " + i
-        }
-        return  e>=18
+btnReduzir.addEventListener("click", (evt) => {
+    dobro.push(elementos_array[0] * 2)
+    resultado.innerHTML=elementos_array.reduce((anterior, atual, pos) => {
+        ant.push(anterior)
+        ant.push(atual)
+        dobro.push(atual * 2)
+        return atual + anterior
     })
-    if(ret) {
-        resultado.innerHTML = "OK"
-    }
-    //console.log(ret)
+    resultado.innerHTML += "<br />V.anterior: " + aux + "<br/>V.atual" + atu + "<br /> Dobro:"+dobro
+
 })
 
 
 /* 
 
-vai dar OK
+Reduce : faz redução do array, no codigo especifico, mesma coida de find, every,
+some... Quem ordena oque vai mudar é o programador, importante destacar novo parametro, 
+permite ser diferente de outros metodos.
 
+funcao calback opera com um parametro novo, o anterior previousvalue, depois é o mesmo
+da anteriores currentvalue valor atual interado, currente index posição atual, e qual array vai ser interado.
+
+previousvalue valor atual e anterior mais não anterior array
+no valor anterior temos nada é 0 ! + 1 é 1!
+atual é resultado soma 1 + 2 , 3 + 3 6,  6 + 4...
 */
 
 
