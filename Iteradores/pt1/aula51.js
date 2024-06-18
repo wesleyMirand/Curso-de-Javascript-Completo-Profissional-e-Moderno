7dbc54abdef163d5696e5bc25e8952376121cdc6
WesleyMirand <wewesantos@hotmail.com>
2024-06-18 12:23:59 -0300

    2

--- Iteradores/pt1/aula51.js
+++ Iteradores/pt1/aula51.js
@@ -1,39 +1,37 @@
-const valores = [10, 9, 8, 2, 6,5, 12, 21]
+const valores = [10, 9, 8, 2]
 const it_valores = valores [Symbol.iterator]()
 
-console.log(valores)
-console.log(it_valores.next())
-console.log(it_valores.next())
-console.log(it_valores.next())
-console.log(it_valores.next())
-console.log(it_valores.next())
-console.log(it_valores.next())
-console.log(it_valores.next())
-console.log(it_valores.next())
+const texto = "Youtube"
+const it_texto = texto [Symbol.iterator]()
+
+console.log(texto)
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+
+
 /* 
 
-Iteradores:find map some every reduce, iteradores como operam e retornam ele, 
-e sabe que a funcao chega no final.
-Em js varias coleçoes e varios iteradas, array, string, map e sets,  elas permitem
-trabalhar retornando interadores que facilitam trabalhar com esses iterador.
+Intera colecoes com arrays, String são colecoes interaveis, 
+const texto = "Youtube"
+const it_texto = texto [Symbol.iterator]() 
+INTERATOR da coleção texto.
 
-const it_valores = [valores [Symbol.iterator]]() = todo interador ja tem o symbol.
-console.log(it_valores.next())= next usa o tempo todo quando estamos interando um array.
-value : primeira posição do array , done : false indica que ele nao chegou no final, forma
-de saber se chegou no final ou não.
+console.log(texto)
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+console.log(it_texto.next())
+ elemento por elemento : Y , O , U ....
+ até chegar no fim
 
-console.log(valores) valores
-console.log(it_valores.next().value) -primeiro valor
-console.log(it_valores.next().value) - proximo valor
-console.log(it_valores.next().value)  -prox
-console.log(it_valores.next().value) - prox
-console.log(it_valores.next().value) -value undefined, done:true OPA ACABOU! chegou no final parar de interar.
-console.log(it_valores.next())
-
-Funciona assim!
-funcoes obtem o valor atual do array e ja sabem com next quando chegou no final.
-
-com o value vai direto pro valor e depois fica undefined quando chegar no fim.
-Com isso ja posso ter um while ou for da na mesma. Enquanto nao for true vou percorrendo.
-O interador faz isso porem ele resume a operacao.
 */
