6b606ba348912c56f697d9d5fdc1cf2352b4688d
WesleyMirand <wewesantos@hotmail.com>
2024-06-20 23:57:53 -0300

    1

---
+++ Objetos/aula  58/pt1/aula57.js
@@ -0,0 +1,19 @@
+class  Carro {
+    constructor (pnome) { 
+        this.nome = pnome 
+    }
+}       
+
+let c1 = new Carro("Bruno") 
+let c2 = new Carro(10) 
+let c3 = new Carro("Lucas")
+
+console.log(p1.nome) 
+console.log(p2.nome)
+console.log(p3.nome) 
+
+/* 
+
+criar propriedades inves de metodos 
+
+*/
