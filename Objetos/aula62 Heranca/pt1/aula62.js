2ad68389d97af9b27522ea03dabc2bd7e110dcbc
WesleyMirand <wewesantos@hotmail.com>
2024-06-27 20:38:19 -0300

    5

--- Objetos/aula62 Heranca/pt1/aula62.js
+++ Objetos/aula62 Heranca/pt1/aula62.js
@@ -17,14 +17,46 @@ class Carro { //Classe pai
     }
 }
 
-class Militar extends Carro {
-
+class Militar extends Carro { //classe Filho
+    constructor ( nome, portas, blindagem, municao) {
+        super(nome, portas)
+        this.blindagem = blindagem
+        this.municao = municao
+        this.setCor = ("Verde")
+    }
+    atirar = function () {
+        if(this.municao > 0) {
+            this.municao--
+        }
+    }
+}
+class Utilitario extends Carro {
+    constructor(nome, portas, lugares){
+        super(nome, portas)
+        this.lugares = lugares
+    }
 }
 
 const c1 = new Carro ("Normal", 4)
 c1.ligar()
 c1.setCor("Preto")
 
-const c2 = new Militar()
+const c2 = new Militar("lutador", 1, 100, 50)
+//c2.setCor ("Azul")
 
 console.log(`Nome : ${c1.nome}`)
+console.log(`Portas : ${c1.portas}`)
+console.log(`Ligado : ${(c1.ligado ? "Sim" : "Não")}`)
+console.log(`Velocidade : ${c1.vel}`)
+console.log(`Cor : ${c1.cor} `)
+console.log(`Lugares : ${c1.lugares} `)
+console.log("---------------------------------------")
+
+console.log(`Nome : ${c2.nome}`)
+console.log(`Portas : ${c2.portas}`)
+console.log(`Ligado : ${(c2.ligado ? "Sim" : "Não")}`)
+console.log(`Velocidade : ${c2.vel}`)
+console.log(`Blindagem : ${c2.blindagem}`)
+console.log(`Munição : ${c2.municao}`)
+console.log(`Cor : ${c2.cor} `)
+console.log("---------------------------------------")
