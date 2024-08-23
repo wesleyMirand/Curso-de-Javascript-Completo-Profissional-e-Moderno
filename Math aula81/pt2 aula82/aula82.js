86d13f9ea4a249b474999a316b69b9d85166874d
WesleyMirand <wewesantos@hotmail.com>
2024-08-22 22:03:31 -0300

    1

---
+++ Math aula81/pt2 aula82/aula82.js
@@ -0,0 +1,16 @@
+const olhos = [...document.getElementsByClassName("olho")]
+
+let posx_mouse=0
+let posy_mouse=0
+
+window.addEventListener("mousemove", (evt) => {
+    posx_mouse = evt.clientX
+    posy_mouse = evt.clientY
+
+    const rot= Math.atan2(posy_mouse, posx_mouse) * 180/Math.PI
+
+    olhos.forEach((0) => {
+        o.style.transform="rotate("+rot+"deg)"
+    })
+})
+
