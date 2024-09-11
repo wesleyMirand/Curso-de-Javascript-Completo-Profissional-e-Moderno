ddf8f69e1546bcf453650b99548142a29a6cd806
WesleyMirand <wewesantos@hotmail.com>
2024-09-10 23:53:32 -0300

    1

---
+++ classes pratica/pt1/aula83.js
@@ -0,0 +1,8 @@
+//objetos
+const palco = document.getElementById("palco")
+const num_objetos = document.getElementById("num_objetos")
+const txt_qtde = document.getElementById("txt_qtde")
+const btn_add = document.getElementById("btn_add")
+const btn_remover = document.getElementById("btn_remover")
+
+//alguma variaveis
