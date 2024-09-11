3459841e3545fb3ce44182f4cbd2c029e3f2582d
WesleyMirand <wewesantos@hotmail.com>
2024-09-11 00:13:54 -0300

    1

--- classes pratica/pt1/aula83.js
+++ classes pratica/pt1/aula83.js
@@ -5,4 +5,9 @@ const txt_qtde = document.getElementById("txt_qtde")
 const btn_add = document.getElementById("btn_add")
 const btn_remover = document.getElementById("btn_remover")
 
+
 //alguma variaveis
+let larguraPalco= palco.offsetWidth
+let alturaPalco=palco.offsetHeight
+let bolas = [] //objetos nesse arrays nesse conteiner
+let numBola = 0
