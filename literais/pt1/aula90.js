8a67f7955e2576df745701c2340c7bdaf93b6b97
WesleyMirand <wewesantos@hotmail.com>
2024-11-18 10:49:49 -0300

    1

--- literais/pt1/aula90.js
+++ literais/pt1/aula90.js
@@ -1,24 +1,49 @@
-const btn_imp=document.getElementById("btn_imp")
+const objetos = document.getElementById("objetos") //literais tipo personalizado classes propriedades e metodos sem instaciar
 
-btn_imp.addEventListener("click",(evt)=>{
-    const conteudo = document.getElementById('tabela').innerHTML; //conteudo do id tabela para constante conteudo
+const computador={
+    cpu:"i9", 
+    ram:"64gb",
+    hd:"2tb", //criar metodo:
+    info:function(){
+        console.log(`CPU:${this.cpu}`)
+        console.log(`RAM:${this.ram}`)
+        console.log(`HD.:${this.hd}`)
+    }
+}
+computador["monitor"] = "22pol"
+computador.placaVideo = "rtx"
 
-    let estilo = "<style>"; //var estilo  incorporado ou inline
-    estilo += "table(width: 100%; font: 25px Calibri;)";
-    estilo += "table, th, td (border: solid 2px #888; border-collapse: collapse;"; //concatenando += formatacao bordas
-    estilo += "padding: 4px 8px; text-align:center;}";
-    estilo += "</style>"; //fecho style podia ser tudo em uma linha so
+console.log(computador["ram"])
 
-    const win = window.open('', '', 'height=700, width = 700'); //nova tamanho da janela , dentro da constante win
-    window.op
+const computadores = [
+    {
+        cpu:"i9", 
+        ram:"64gb",
+        hd:"2tb"
+    },
+    {
+        cpu:"i7", 
+        ram:"32gb",
+        hd:"2tb"
+    },
+    {
+        cpu:"i5", 
+        ram:"16gb",
+        hd:"1tb"
+    },
+]
 
-    win.document.write('<html><head>'); // da janela que criei chamei documente write  com codigo html, pagina normal
-    win.document.write('<title>Wewe Cursos</title>');
-    win.document.write(estilo); //incorporado, associei o style
-    win.document.write('</head>');
-    win.document.write('<body>')
-    win.document.write(conteudo); //minha tabela associar a constante conteudo
-    win.document.write('</body></html>');  //finalizei o body e html aberto
-
-    win.print() //depois um print, imprime o conteudo
+computadores.forEach((c) => {
+    console.log(c)
+    const div = document.createElement("div")
+    div.innerHTML = c.cpu+"<br/>"+c.ram + "<br/>"+ c.hd
+    div.setAttribute("class", "computador")
+    objetos.appendChild(div)
 })
+
+
+//computador.info()
+
+//console.log(computadores)
+//objetos.innerHTML = computador.cpu + " - " + computador.ram + " - " + computador.hd
+//objetos.innerHTML = JSON.stringify(computadores)
